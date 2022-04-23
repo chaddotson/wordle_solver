1d95ac3100a7a34b8b22fdb3c26f9fedc6b356e3
Chad Dotson <1754094+chaddotson@users.noreply.github.com>
2022-04-23 11:29:01 -0500

    Revert code back following q&d changes to new word list.

--- wordinfo/utils.py
+++ wordinfo/utils.py
@@ -9,9 +9,8 @@ DATA_PATH = Path(__file__).parent / 'data'
 class WordSource(Enum):
     WORDLE_SOLUTIONS = DATA_PATH / 'wordle_solutions.txt'
     WORDLE_FULL = DATA_PATH / 'wordle_full.txt'
-    NYT_WORDLE_SOLUTIONS = DATA_PATH / 'nyt_wordle_solutions.txt'
-    NYT_WORDLE_FULL = DATA_PATH / 'nyt_wordle_full.txt'
-
+    NYT_WORDLE_SOLUTIONS = DATA_PATH / 'new_nyt_wordle_solutions.txt'
+    NYT_WORDLE_FULL = DATA_PATH / 'new_nyt_wordle_full.txt'
 
 
 def load_word_list(wordset=WordSource.NYT_WORDLE_SOLUTIONS):
@@ -20,23 +19,25 @@ def load_word_list(wordset=WordSource.NYT_WORDLE_SOLUTIONS):
         return [l.strip('\n').lower() for l in f.readlines() if not regex.search(l) and len(l) == 6]
 
 
+
 def get_word_of_day(wordlist):
     start_date = date(year=2021, month=6, day=19)
     today = date.today()
-    index = (today - start_date).days+1
+    index = (today - start_date).days
     return index, wordlist[index]
+
+
+REPRESENTATION_MAP = {
+    2: '🟩',
+    1: '🟨',
+    0: '⬛'
+}
 
 
 def get_result_representation(results):
     representation = ''
     for r in results:
         for i in r:
-            if i == 2:
-                representation += '🟩'
-            elif i == 1:
-                representation += '🟨'
-            else:
-                representation += '⬛'
-        else:
-            representation += '\r\n'
+            representation += REPRESENTATION_MAP[i]
+        representation += '\r\n'
     return representation
