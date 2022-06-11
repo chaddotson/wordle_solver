bfe2369da53b81249a83ee364934e877be06f58d
Chad Dotson <1754094+chaddotson@users.noreply.github.com>
2022-06-11 12:51:59 -0500

    Add docs

--- wordinfo/suggesters/utils.py
+++ wordinfo/suggesters/utils.py
@@ -2,6 +2,12 @@ import re
 
 
 def generate_regex_for_eliminations(bad_words, letter_tracker):
+    """
+    Generate the regex for attempting to eliminate as many letters as possible.
+    :param bad_words: Words that have been tried.
+    :param letter_tracker: Object tracking attempts and results.
+    :returns: regex string
+    """
     used_letters = [letter for word in bad_words for letter in word]
     used_letters.extend(letter_tracker.invalids)
     used_letters.extend(set(letter for not_at_list in letter_tracker.not_at.values() for letter in not_at_list))
@@ -11,11 +17,16 @@ def generate_regex_for_eliminations(bad_words, letter_tracker):
     invalids_regex = ''.join(f'(?!.*{invalid})' for invalid in used_letters)
     invalid_words_regex = ''.join(f'(?!{word})' for word in bad_words)
     total_regex = f'^{invalid_words_regex}{invalids_regex}{contains_regex}(?:{letters_regex})$'
-
     return total_regex
 
 
 def generate_regex(bad_words, letter_tracker):
+    """
+    Generate the regex for identifying possible solutions.
+    :param bad_words: Words that have been tried.
+    :param letter_tracker: Object tracking attempts and results.
+    :returns: regex string
+    """
     letters_regex = ''.join([f'{"".join(f"(?!{nat})" for nat in letter_tracker.not_at.get(index, []))}[a-z]' if letter_tracker.at[index] is None else letter_tracker.at[index] for index in range(len(letter_tracker.at))])
     contains_regex = ''.join(f'(?=.*{letter})' for position_not_ats in letter_tracker.not_at.values() for letter in position_not_ats)
     invalids_regex = ''.join(f'(?!.*{invalid})' for invalid in letter_tracker.invalids)
@@ -25,7 +36,12 @@ def generate_regex(bad_words, letter_tracker):
 
 
 def generate_suggestion_by_ranked(ranked, regex):
-    # print(regex)
+    """
+    Find the best word given the ranked list that fits the regex.
+    :param ranked:
+    :param regex:
+    :return:
+    """
     # try:
     suggestion, _ = next(pair for pair in ranked if re.search(regex, pair[0]))
     # except StopIteration:
