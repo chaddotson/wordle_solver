8e760caa13eb178ef5995086d59a0113fedfd772
Chad Dotson <1754094+chaddotson@users.noreply.github.com>
2023-09-10 11:42:09 -0500

    Add culling support to hybrid methods.

--- wordinfo/suggesters/hybrid.py
+++ wordinfo/suggesters/hybrid.py
@@ -2,17 +2,19 @@ from pathlib import Path
 
 from wordinfo.suggesters.base import Suggester
 from wordinfo.suggesters.dominance import DominanceEliminationSuggester
-from wordinfo.suggesters.entropy import PopularEntropySuggester
+from wordinfo.suggesters.entropy import EntropySuggester, PopularEntropySuggester
 from wordinfo.suggesters.rank import RankEliminationSuggester
+from wordinfo.solver import LetterTracker
 
 
 class DominananceEntropyEliminationSuggester(Suggester):
-    __pretty_name__ = 'Dominance Entropy Elimination {elimination_attempts}'
+    __pretty_name__ = 'Dominance Entropy Elimination {elimination_attempts}/{cull}'
 
-    def __init__(self, wordlist, cache_path: Path, word_frequency_map,  elimination_attempts=3, *args, **kwargs):
+    def __init__(self, wordlist, cache_path: Path, word_frequency_map,  elimination_attempts=3, cull=40, *args, **kwargs):
         self.elimination_attempts = elimination_attempts
+        self.cull = cull
         self._domination_suggester = DominanceEliminationSuggester(wordlist, elimination_attempts)
-        self._entropy_suggester = PopularEntropySuggester(wordlist, cache_path, word_frequency_map)
+        self._entropy_suggester = PopularEntropySuggester(wordlist, cache_path, word_frequency_map, cull)
 
     def get_suggestion(self, attempt, attempt_words, letter_tracker):
         if len(attempt_words) < self.elimination_attempts:
@@ -21,12 +23,13 @@ class DominananceEntropyEliminationSuggester(Suggester):
 
 
 class RankEntropyEliminationSuggester(Suggester):
-    __pretty_name__ = 'Rank Entropy Elimination {elimination_attempts}'
+    __pretty_name__ = 'Rank Entropy Elimination {elimination_attempts} {cull}'
 
-    def __init__(self, wordlist, cache_path: Path, word_frequency_map,  elimination_attempts=3, *args, **kwargs):
+    def __init__(self, wordlist, cache_path: Path, word_frequency_map,  elimination_attempts=3, cull=40, *args, **kwargs):
         self.elimination_attempts = elimination_attempts
+        self.cull = cull
         self._rank_suggester = RankEliminationSuggester(wordlist, elimination_attempts)
-        self._entropy_suggester = PopularEntropySuggester(wordlist, cache_path, word_frequency_map)
+        self._entropy_suggester = PopularEntropySuggester(wordlist, cache_path, word_frequency_map, cull)
 
     def get_suggestion(self, attempt, attempt_words, letter_tracker):
         if len(attempt_words) < self.elimination_attempts:
